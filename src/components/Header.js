a531cd4dc250121146aad5f04be950110f785520
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-05-18 16:45:51 -0400

    styling buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 function Header() {
   return (
-    <header >
-      <h1>CV-Generator</h1>
+    <header>
+      <div id="content-box">
+        <h1>CV-Generator</h1>
+        <button id="update-general" name="update general" className="head-btn">
+          Update General Information
+        </button>
+        <button id="add-education" name="add education" className="head-btn">
+          Add Education
+        </button>
+        <button id="add-experience" name="add experience" className="head-btn">
+          Add Experience
+        </button>
+      </div>
     </header>
   );
 }
